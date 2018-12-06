24fdd0d1e45a052b47e2bc322dde43dfb001729e
Rick Hunter <rickhunterec@msn.com>
2018-12-06 08:42:13 +0100

    Add Greeting Class
    [ticket: X]
    
    	modified:   src/main/java/helloworldmuleproject/Greeting.java

--- src/main/java/helloworldmuleproject/Greeting.java
+++ src/main/java/helloworldmuleproject/Greeting.java
@@ -1,5 +1,14 @@
 package helloworldmuleproject;
 
-public class Greeting {
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+
+public class Greeting implements Callable {
+
+	@Override
+	public Object onCall(MuleEventContext eventContext) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
